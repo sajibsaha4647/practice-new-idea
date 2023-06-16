89e4494316a2bd13472f409a895a3d94df206590
sajibsaha4647 <sksaha4648@gmail.com>
2023-06-16 16:41:46 +0600

    added variable data type

---
+++ src/src/VariableDatatype.java
@@ -0,0 +1,30 @@
+package src;
+
+public class VariableDatatype {
+
+    public static void main(String[] args) {
+        //here is data types
+        boolean b;
+        char c;
+        short s;
+        int i;
+        float f;
+        double d;
+
+        b = false;
+        c = 'a';
+        s = 12232;
+        i = 132123123;
+        f = 23324.5f;
+        d = 34.3;
+
+
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+
+    }
+}
