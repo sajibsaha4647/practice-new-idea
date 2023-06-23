22ee300b1a9c4a7f991ce05f725624e3b27504ee
sajibsaha4647 <sksaha4648@gmail.com>
2023-06-23 14:42:54 +0600

    added unary operator

---
+++ src/src/unaryoperator.java
@@ -0,0 +1,24 @@
+package src;
+
+public class unaryoperator {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int result,result2;
+
+        result = x++ ;
+
+        System.out.println(result);//10
+
+        result = x; //11
+
+        System.out.println(result);//11
+
+
+
+
+
+    }
+
+}
