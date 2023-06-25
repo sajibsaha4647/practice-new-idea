4e953f0009a43ffa3d808f53a392aab8acac3b68
sajibsaha4647 <sksaha4648@gmail.com>
2023-06-25 13:39:17 +0600

    added continue and break

---
+++ src/src/ breakandcontinue.java
@@ -0,0 +1,15 @@
+package src;
+
+class  breakandcontinue {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i<=100 ; i++){
+            if(i == 10){
+                // continue;
+                // break;
+            }
+            System.out.println("print "+i);
+        }
+
+    }
+}
