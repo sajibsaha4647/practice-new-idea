bcf76e6cdcc1e27527ec3cff61f9010d0c3e88fa
sajibsaha4647 <sksaha4648@gmail.com>
2023-06-26 10:25:29 +0600

    fibonacci

--- src/src/FebonacciSeries.java
+++ src/src/FebonacciSeries.java
@@ -1,10 +1,35 @@
 package src;
 
+import java.util.Scanner;
+
 public class FebonacciSeries {
 
     public static void main(String[] args) {
         
-        
+        Scanner input = new Scanner(System.in);
+
+        int n,first=0,second=1,fibo;
+
+        System.out.println("enter positive number");
+
+        n = input.nextInt();
+
+        System.out.println(first+""+second);
+
+        for(int i = 3;i<=n;i++){
+
+            fibo = first+second;
+
+            System.out.println(" "+fibo);
+
+            first = second;
+
+            second = fibo;
+
+
+        }
+
+
 
     }
     
