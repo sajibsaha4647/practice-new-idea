460e6dc7c69791f3f5c410db8aa608c2dcf1a563
sajibsaha4647 <sksaha4648@gmail.com>
2023-08-06 18:57:59 +0600

    added string Builder

---
+++ src/StirngsMethods/StringBuilders.java
@@ -0,0 +1,16 @@
+package StirngsMethods;
+
+public class StringBuilders {
+
+    public static void main(String[] args) {
+
+
+        StringBuilder strt = new StringBuilder();
+ 
+        // strt.append("sajib").toString();
+ 
+        // print string
+        // System.out.println("String = " + str.toString());
+    }
+    
+}
