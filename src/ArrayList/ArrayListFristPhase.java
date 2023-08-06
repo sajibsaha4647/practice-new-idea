3031061a38ee0ee8e7c9bc32b57fe2bf5085a9bd
sajibsaha4647 <sksaha4648@gmail.com>
2023-08-06 16:02:51 +0600

    initial completed

---
+++ src/ArrayList/ArrayListFristPhase.java
@@ -0,0 +1,33 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListFristPhase {
+
+    public static void main(String[] args) {
+        ArrayList<ArraylistModel>  number = new ArrayList<ArraylistModel>();
+
+    //   ArraylistModel modelname =  new ArraylistModel("sajib",23);
+
+
+      for(int i = 0; i<10;i++){
+        // ArrayList<ArraylistModel>  number = new ArrayList<ArraylistModel>();
+
+        // number.add( new ArraylistModel("sajib",i));
+      }
+
+
+        number.add( new ArraylistModel("sajib",12));
+
+
+    //   number.add(modelname);
+
+    //     int sajib =  number.get(0).age ;
+
+    //     System.out.println(sajib);
+
+    }
+
+    
+    
+}
---
+++ src/ArrayList/ArraylistModel.java
@@ -0,0 +1,31 @@
+package ArrayList;
+
+public class ArraylistModel {
+    String name;
+    int age;
+
+   public ArraylistModel(String name,int age){
+            this.name = name ;
+            this.age = age;
+    }
+
+   public String getName(){
+    return  name;
+   }
+
+   public String setName(String name){
+    return  name;
+   }
+
+   public int getAge(){
+    return  age;
+   }
+
+   public int setAge(int age){
+    return  age;
+   }
+
+
+
+
+}
