24f564118e13e88a2fb16a0c484fca73ecf309a1
sajibsaha4647 <sksaha4648@gmail.com>
2023-08-10 11:10:01 +0600

    using tostring class

---
+++ src/Wrapperclass/TostringConversion.java
@@ -0,0 +1,19 @@
+package Wrapperclass;
+
+public class TostringConversion {
+
+    public static void main(String[] args) {
+
+        Double i = 12.20 ;
+        String s = Double.toString(i);
+
+        String ss = "32" ;
+        Integer in = Integer.parseInt(ss);
+        System.out.println(ss);
+        System.out.println(s);
+
+
+        
+    }
+    
+}
