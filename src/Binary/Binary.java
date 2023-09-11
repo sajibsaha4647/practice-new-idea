ab5e31f27ed4019bdee484da8a08db79fbe60be3
sajibsaha4647 <sksaha4648@gmail.com>
2023-09-12 00:27:08 +0600

    binary options

--- src/Binary/Binary.java
+++ src/Binary/Binary.java
@@ -1,9 +1,28 @@
 package Binary;
 
+import java.util.Scanner;
+
 public class Binary {
 
     public static void main(String[] args) {
-        System.out.println("check binary");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("please enter a number");
+        int num = input.nextInt();
+
+        String value = "101010";
+
+        // binary to decimal
+        Integer decimal = Integer.parseInt(value, 2);
+        System.out.println(decimal);
+
+        // Decimal To Binary, octal, hexadecimal
+        int decim = 15;
+
+        String binaryString = Integer.toBinaryString(decim);
+        String hexString = Integer.toHexString(decim);
+
+        System.out.println(hexString);
     }
 
 }
